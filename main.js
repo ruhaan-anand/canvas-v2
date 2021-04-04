1390dc236c1be60cfd367aac4754885bae588173
ruhaan-anand <73781091+ruhaan-anand@users.noreply.github.com>
2021-04-04 17:49:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function clear_screen(){
     pen.clearRect(0,0,canvas.width,canvas.height);
 }
+function setDimensions(){
+    if(screen.width<=428){
+        canvas.width=screen.width;
+        document.body.style.overflow="hidden"
+    }
+}
 var canvas=document.getElementById('myCanvas')
 var pen = canvas.getContext("2d");
 
@@ -26,16 +32,40 @@ window.addEventListener('mousedown',function(){
   tracker = 1  
 }
 )
+window.addEventListener('touchstart',function(event){
+    x2=event.touches[0].clientX-canvas.offsetLeft;
+    y2=event.touches[0].clientY-canvas.offsetTop;
+}
+)
 window.addEventListener('mouseup',function(){
    tracker = 0 
 }
 )
+
 window.addEventListener('mousemove',function(event){
     console.log(event);
     x1=event.clientX-canvas.offsetLeft;
     y1=event.clientY-canvas.offsetTop;
     
     if (tracker == 1){
+        pen.beginPath();
+        pen.lineWidth=3;
+        pen.strokeStyle=document.getElementById("colour").value;
+        pen.moveTo(x1,y1)
+        pen.lineTo(x2,y2)
+        pen.stroke(); 
+    }
+
+    x2 = x1
+    y2 = y1
+}
+)
+window.addEventListener('touchmove',function(event){
+    console.log(event);
+    x1=event.touches[0].clientX-canvas.offsetLeft;
+    y1=event.touches[0].clientY-canvas.offsetTop;
+    
+     if (tracker == 1){
         pen.beginPath();
         pen.lineWidth=3;
         pen.strokeStyle=document.getElementById("colour").value;
